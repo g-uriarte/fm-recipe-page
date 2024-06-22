231a1f734328f2b41147746b73f9e5b2a9c44bc3
g-uriarte <gabriel.agustin.uriarte@gmail.com>
2024-06-22 16:52:39 -0300

    created structure and added styles

--- src/components/Ingredients.tsx
+++ src/components/Ingredients.tsx
@@ -6,11 +6,18 @@ type IngredientsProps = {
 
 export const Ingredients: FC<IngredientsProps> = ({
     ingredients
- }) => {
+}) => {
 
-    {/* Ingredients */}
-                    {/* List n items [dots] */}
-                    {/* Separator */}
-
-    return <div>Ingredientes</div>
+    return (
+        <section className='flex flex-col gap-3' >
+            <h3 className='text-Nutmeg font-Young-Serif text-2xl'>Ingredients</h3>
+            <ul className='marker:text-Dark-Raspberry list-inside list-none' >
+                {ingredients.map(ingredient => (
+                    <li className='py-0.5 before:pl-2 before:content-["•"] before:text-Dark-Raspberry before:items-center before:flex flex'>
+                        <p className='pl-5 text-sm font-Outfit text-Wenge-Brown'>{ingredient}</p>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    )
 }
