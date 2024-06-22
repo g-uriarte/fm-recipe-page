898cd736b2627f927bcd55919e08d97622a69148
g-uriarte <gabriel.agustin.uriarte@gmail.com>
2024-06-22 17:36:23 -0300

    created instructions section

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -5,8 +5,30 @@ type InstructionsProps = {
     steps: Instruction[]
 }
 
-export const Instructions: FC<InstructionsProps> = ({ }) => {
-    {/* Instructions */ }
-    {/* List n [numbers]*/ }
-    return <div>Instructions</div>
+const ListItem: FC<{ step: Instruction }> = ({
+    step
+}) => {
+    return (
+        <li className="flex py-0.5 [counter-increment:section] before:text-Dark-Raspberry before:font-semibold before:text-sm before:pl-2 before:[content:counter(section)'.']">
+            <p className='pl-5 text-sm font-Outfit text-Wenge-Brown'>
+                <span className='mr-2 font-semibold'>{step.title}:</span>
+                {step.description}
+            </p>
+        </li>
+    )
+}
+
+export const Instructions: FC<InstructionsProps> = ({
+    steps
+}) => {
+    return (
+        <section className='flex flex-col gap-3'>
+            <h3 className='text-Nutmeg font-Young-Serif text-2xl'>Instructions</h3>
+            <ol className='list-none list-inside'>
+                {steps.map(step => (
+                    <ListItem step={step} />
+                ))}
+            </ol>
+        </section>
+    )
 }
