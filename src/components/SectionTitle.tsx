f1da1619211c42043da7a9f45a5965c904e53cff
g-uriarte <gabriel.agustin.uriarte@gmail.com>
2024-06-23 14:09:28 -0300

    fixed accesibility (Heading levels should only increase by one)

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -4,4 +4,4 @@ type SectionTitleProps = {
     title: string;
 }
 
-export const SectionTitle: FC<SectionTitleProps> = ({ title }) => <h3 className='text-Nutmeg font-Young-Serif text-2xl'>{title}</h3>
+export const SectionTitle: FC<SectionTitleProps> = ({ title }) => <h2 className='text-Nutmeg font-Young-Serif text-2xl'>{title}</h2>
