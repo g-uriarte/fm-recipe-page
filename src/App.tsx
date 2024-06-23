89a6938ca2b9b392428d2e7da9fd4d9cf070552f
g-uriarte <gabriel.agustin.uriarte@gmail.com>
2024-06-23 13:36:39 -0300

    configure base public path

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { RecipeContainer } from "./components/RecipeContainer"
-import { RECIPE } from "./data/receiptData"
+import { RECIPE } from "./data/recipe"
 
 export const App = () => {
 	return (
-		<div className="h-100dvh w-100dvw flex justify-center items-center content-center bg-Eggshell xl:h-[100%]">
+		<div className="h-100dvh w-100dvw flex justify-center items-center content-center bg-Eggshell xl:h-[100%] xl:w-full">
 			<RecipeContainer recipe={RECIPE} />
 		</div>
 	)
